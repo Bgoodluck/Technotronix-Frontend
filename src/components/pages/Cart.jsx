477292493b515f6b5be39006fe2d70416fa628ff
unknown <Bgoodluck>
2024-06-27 11:48:04 +0100

    again

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,4 +1,91 @@
-import React from 'react'
+// import React from 'react'
+// import { MdDeleteForever } from "react-icons/md";
+// import { useContext } from 'react';
+// import EcomContext from '../../context/EcomContext';
+// import { Link } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
+
+// function Cart() {
+// const { cartItems, updateQuantity, removeItem, totalAmount, isAuthenticated } = useContext(EcomContext);
+
+// if (!isAuthenticated) {
+//    return <Navigate to='/login' /> 
+// }
+
+
+
+
+// // const handleDelete = () => {
+// //     removeFromCart(item.Id);
+// // };
+
+
+// const cartTable = (
+//     <>
+//         <table className='w-[90%] mx-auto h-[30vh]'>
+//         <thead>
+//             <th>Action</th>
+//             <th>Item</th>
+//             <th>Image</th>
+//             <th>Price</th>
+//             <th>Quantity</th>
+//             <th>Amount</th>
+//         </thead>
+//         <tbody className='text-center'>
+//             {cartItems.products?.map((item) => (
+//                 <tr className='border-b-2' key={item.product._id}>
+//                     <td>
+//                         <div>
+//                             <button onClick={()=> removeItem(item.product._id)}>
+//                             <MdDeleteForever className='text-2xl text-orange-500' />
+//                             </button>
+//                         </div>
+//                     </td>
+//                     <td>{item.product.name}</td>
+//                     <td>
+//                         <div className='flex justify-center'>
+//                             <img src={"https://technotronix-api-eeve.onrender.com" +  item.product.img} className='h-[50px]' alt="" />
+//                         </div>
+//                     </td>
+//                     <td>₦{item.products?.price}</td>
+//                     <td>
+//                         <input type="number" className='outline outline-1 w-[50px]'
+//                         value={item.quantity} 
+//                         min={1}
+//                         onChange={(e)=> updateQuantity(item.product._id, e.target.value)} />
+//                     </td>
+//                     <td>₦{item.amount}</td>
+//                 </tr>
+//             ))}
+//         </tbody>
+//     </table>
+
+//     <div className='w-[90%] mx-auto mt-5 flex justify-between'>
+//         <div>
+//             <h1 className='text-4xl font-bold'>Total = ₦{totalAmount()}</h1>
+//         </div>
+//         <div>
+//             <Link to="/checkout">
+//             <button className='bg-orange-500 text-white p-[10px] rounded-md'>Checkout</button>
+//             </Link>
+//         </div>
+//     </div>
+//     </>
+// );
+
+
+
+//   return (
+//     <div className='m-[5%]'>
+//         <h1 className='text-3xl font-bold text-center mb-10'>Your Cart Items</h1>
+//         {cartItems.products?.length > 0 ? cartTable :  <h1 className='text-center font-bold'>No Items in Cart</h1> }
+//     </div>
+//   )
+// }
+
+// export default Cart
+
+import React from 'react';
 import { MdDeleteForever } from "react-icons/md";
 import { useContext } from 'react';
 import EcomContext from '../../context/EcomContext';
@@ -6,81 +93,76 @@ import { Link } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 
 function Cart() {
-const { cartItems, updateQuantity, removeItem, totalAmount, isAuthenticated } = useContext(EcomContext);
+  const { cartItems, updateQuantity, removeItem, totalAmount, isAuthenticated } = useContext(EcomContext);
 
-if (!isAuthenticated) {
-   return <Navigate to='/login' /> 
-}
+  if (!isAuthenticated) {
+    return <Navigate to='/login' />;
+  }
 
-
-
-
-// const handleDelete = () => {
-//     removeFromCart(item.Id);
-// };
-
-
-const cartTable = (
+  const cartTable = (
     <>
-        <table className='w-[90%] mx-auto h-[30vh]'>
-        <thead>
-            <th>Action</th>
-            <th>Item</th>
-            <th>Image</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Amount</th>
-        </thead>
-        <tbody className='text-center'>
+      <div className="overflow-x-auto">
+        <table className="min-w-full mx-auto h-[30vh]">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Action</th>
+              <th className="px-4 py-2">Item</th>
+              <th className="px-4 py-2">Image</th>
+              <th className="px-4 py-2">Price</th>
+              <th className="px-4 py-2">Quantity</th>
+              <th className="px-4 py-2">Amount</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
             {cartItems.products?.map((item) => (
-                <tr className='border-b-2' key={item.product._id}>
-                    <td>
-                        <div>
-                            <button onClick={()=> removeItem(item.product._id)}>
-                            <MdDeleteForever className='text-2xl text-orange-500' />
-                            </button>
-                        </div>
-                    </td>
-                    <td>{item.product.name}</td>
-                    <td>
-                        <div className='flex justify-center'>
-                            <img src={"https://technotronix-api-eeve.onrender.com" +  item.product.img} className='h-[50px]' alt="" />
-                        </div>
-                    </td>
-                    <td>₦{item.products?.price}</td>
-                    <td>
-                        <input type="number" className='outline outline-1 w-[50px]'
-                        value={item.quantity} 
-                        min={1}
-                        onChange={(e)=> updateQuantity(item.product._id, e.target.value)} />
-                    </td>
-                    <td>₦{item.amount}</td>
-                </tr>
+              <tr className="border-b-2" key={item.product._id}>
+                <td className="px-4 py-2">
+                  <button onClick={() => removeItem(item.product._id)}>
+                    <MdDeleteForever className="text-2xl text-orange-500" />
+                  </button>
+                </td>
+                <td className="px-4 py-2">{item.product.name}</td>
+                <td className="px-4 py-2">
+                  <div className="flex justify-center">
+                    <img src={"https://technotronix-api-eeve.onrender.com" + item.product.img} className="h-[50px]" alt="" />
+                  </div>
+                </td>
+                <td className="px-4 py-2">₦{item.product.price}</td>
+                <td className="px-4 py-2">
+                  <input
+                    type="number"
+                    className="outline outline-1 w-[50px]"
+                    value={item.quantity}
+                    min={1}
+                    onChange={(e) => updateQuantity(item.product._id, e.target.value)}
+                  />
+                </td>
+                <td className="px-4 py-2">₦{item.amount}</td>
+              </tr>
             ))}
-        </tbody>
-    </table>
+          </tbody>
+        </table>
+      </div>
 
-    <div className='w-[90%] mx-auto mt-5 flex justify-between'>
-        <div>
-            <h1 className='text-4xl font-bold'>Total = ₦{totalAmount()}</h1>
+      <div className="w-[90%] mx-auto mt-5 flex flex-col md:flex-row justify-between items-center">
+        <div className="mb-4 md:mb-0">
+          <h1 className="text-2xl md:text-4xl font-bold">Total = ₦{totalAmount()}</h1>
         </div>
         <div>
-            <Link to="/checkout">
-            <button className='bg-orange-500 text-white p-[10px] rounded-md'>Checkout</button>
-            </Link>
+          <Link to="/checkout">
+            <button className="bg-orange-500 text-white p-2 md:p-4 rounded-md">Checkout</button>
+          </Link>
         </div>
-    </div>
+      </div>
     </>
-);
-
-
+  );
 
   return (
-    <div className='m-[5%]'>
-        <h1 className='text-3xl font-bold text-center mb-10'>Your Cart Items</h1>
-        {cartItems.products?.length > 0 ? cartTable :  <h1 className='text-center font-bold'>No Items in Cart</h1> }
+    <div className="m-5 md:m-[5%]">
+      <h1 className="text-2xl md:text-3xl font-bold text-center mb-10">Your Cart Items</h1>
+      {cartItems.products?.length > 0 ? cartTable : <h1 className="text-center font-bold">No Items in Cart</h1>}
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -1,117 +1,230 @@
-import React from 'react'
-import { useContext } from 'react'
-import EcomContext from '../../context/EcomContext'
+// import React from 'react'
+// import { useContext } from 'react'
+// import EcomContext from '../../context/EcomContext'
+// import { Navigate } from 'react-router-dom';
+
+
+// function Checkout() {
+
+//     const { cartItems, totalAmount, isAuthenticated } = useContext(EcomContext)
+//     const total = totalAmount();
+
+//     if (!isAuthenticated) {
+//         return <Navigate to='/login' />
+//     }
+
+//     const handleCheckout = async (e) => {
+//         e.preventDefault();
+
+//         const amount = totalAmount()
+//         const currency = "NGN";
+
+//         const firstName = e.target.elements.firstName.value;
+//         const lastName = e.target.elements.lastName.value;
+//         const phone = e.target.elements.phone.value;
+//         const address = e.target.elements.address.value;
+
+//         try {
+//             const res = await fetch("https://technotronix-api-eeve.onrender.com/api/payment/initiate", {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     "auth-token": `${localStorage.getItem("auth-token")}`,
+//                 },
+//                 body: JSON.stringify({ amount, currency, firstName, lastName, phone, address })
+//             })
+
+//             const data = await res.json()
+//             if (res.ok) {
+//                 window.location.href = data.link
+//             } else {
+//                 console.error(data.msg || "Failed to initiate payment");
+//             }
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     }
+
+
+//     return (
+//         <div className='flex flex-col lg:flex-row gap-8 m-[5%] '>
+//             <div className='w-[50%]'>
+//                 <h1 className='font-bold text-center mb-5'>Order Summary</h1>
+//                 <table className='w-[100%] mx-auto h-[50vh]'>
+//                     <thead>
+
+//                         <th>Item</th>
+//                         <th>Image</th>
+//                         <th>Price</th>
+//                         <th>Quantity</th>
+//                         <th>Amount</th>
+//                     </thead>
+//                     <tbody className='text-center'>
+//                         {cartItems.products?.map((item) => (
+//                             <tr className='border-b-2 font-semibold'>
+//                                 <td>{item.name}</td>
+//                                 <td>
+//                                     <div className='flex justify-center'>
+//                                         <img src={"https://technotronix-api-eeve.onrender.com/" + item.product.img} className='h-[50px]' alt="" />
+//                                     </div>
+//                                 </td>
+//                                 <td>₦{item.product.price}</td>
+//                                 <td>
+//                                     {item.quantity}
+//                                 </td>
+//                                 <td>₦{item.amount}</td>
+//                             </tr>
+//                         ))}
+//                     </tbody>
+//                 </table>
+//                 <div className='w-[90%] mx-auto font-bold text-2xl'>
+//                     <h1>Total = ₦{totalAmount()}</h1>
+
+//                 </div>
+//             </div>
+//             <div className='w-[50%] mx-auto font-semibold'>
+//                 <h1 className='text-2xl font-bold'>Delivery Information</h1>
+//                 <form onSubmit={(e) => handleCheckout(e)} id='orderId'>
+//                     <div className='flex flex-col gap-3 mb-3'>
+//                         <label htmlFor="first name"> First Name</label>
+//                         <input className='outline outline-1' type="text" name='firstName' />
+//                     </div>
+//                     <div className='flex flex-col gap-3 mb-3'>
+//                         <label htmlFor="last name"> Last Name</label>
+//                         <input className='outline outline-1' type="text" name='lastName' />
+//                     </div>
+//                     <div className='flex flex-col gap-3 mb-3'>
+//                         <label htmlFor="phone number"> Phone Number</label>
+//                         <input className='outline outline-1' type="text" name='phone' />
+//                     </div>
+//                     <div className='flex flex-col gap-3 mb-3'>
+//                         <label className='font-bold' htmlFor="Address">Address</label>
+//                         <textarea className='outline outline-1' name="address" id="" cols="30" rows="10" ></textarea>
+
+//                     </div>
+//                     <div>
+//                         <button className='bg-black text-white p-[10px] rounded-md hover:bg-orange-500'>
+//                             Pay Now
+//                         </button>
+//                     </div>
+//                 </form>
+//             </div>
+
+//         </div>
+//     )
+// }
+
+// export default Checkout
+
+import React from 'react';
+import { useContext } from 'react';
+import EcomContext from '../../context/EcomContext';
 import { Navigate } from 'react-router-dom';
 
-
 function Checkout() {
+  const { cartItems, totalAmount, isAuthenticated } = useContext(EcomContext);
+  const total = totalAmount();
 
-    const { cartItems, totalAmount, isAuthenticated } = useContext(EcomContext)
-    const total = totalAmount();
+  if (!isAuthenticated) {
+    return <Navigate to='/login' />;
+  }
 
-    if (!isAuthenticated) {
-        return <Navigate to='/login' />
+  const handleCheckout = async (e) => {
+    e.preventDefault();
+
+    const amount = totalAmount();
+    const currency = "NGN";
+
+    const firstName = e.target.elements.firstName.value;
+    const lastName = e.target.elements.lastName.value;
+    const phone = e.target.elements.phone.value;
+    const address = e.target.elements.address.value;
+
+    try {
+      const res = await fetch("https://technotronix-api-eeve.onrender.com/api/payment/initiate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+        },
+        body: JSON.stringify({ amount, currency, firstName, lastName, phone, address })
+      });
+
+      const data = await res.json();
+      if (res.ok) {
+        window.location.href = data.link;
+      } else {
+        console.error(data.msg || "Failed to initiate payment");
+      }
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    const handleCheckout = async (e) => {
-        e.preventDefault();
-
-        const amount = totalAmount()
-        const currency = "NGN";
-
-        const firstName = e.target.elements.firstName.value;
-        const lastName = e.target.elements.lastName.value;
-        const phone = e.target.elements.phone.value;
-        const address = e.target.elements.address.value;
-
-        try {
-            const res = await fetch("https://technotronix-api-eeve.onrender.com/api/payment/initiate", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "auth-token": `${localStorage.getItem("auth-token")}`,
-                },
-                body: JSON.stringify({ amount, currency, firstName, lastName, phone, address })
-            })
-
-            const data = await res.json()
-            if (res.ok) {
-                window.location.href = data.link
-            } else {
-                console.error(data.msg || "Failed to initiate payment");
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-
-    return (
-        <div className='flex flex-col lg:flex-row gap-8 m-[5%] '>
-            <div className='w-[50%]'>
-                <h1 className='font-bold text-center mb-5'>Order Summary</h1>
-                <table className='w-[100%] mx-auto h-[50vh]'>
-                    <thead>
-
-                        <th>Item</th>
-                        <th>Image</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Amount</th>
-                    </thead>
-                    <tbody className='text-center'>
-                        {cartItems.products?.map((item) => (
-                            <tr className='border-b-2 font-semibold'>
-                                <td>{item.name}</td>
-                                <td>
-                                    <div className='flex justify-center'>
-                                        <img src={"https://technotronix-api-eeve.onrender.com/" + item.product.img} className='h-[50px]' alt="" />
-                                    </div>
-                                </td>
-                                <td>₦{item.product.price}</td>
-                                <td>
-                                    {item.quantity}
-                                </td>
-                                <td>₦{item.amount}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <div className='w-[90%] mx-auto font-bold text-2xl'>
-                    <h1>Total = ₦{totalAmount()}</h1>
-
-                </div>
-            </div>
-            <div className='w-[50%] mx-auto font-semibold'>
-                <h1 className='text-2xl font-bold'>Delivery Information</h1>
-                <form onSubmit={(e) => handleCheckout(e)} id='orderId'>
-                    <div className='flex flex-col gap-3 mb-3'>
-                        <label htmlFor="first name"> First Name</label>
-                        <input className='outline outline-1' type="text" name='firstName' />
+  return (
+    <div className='flex flex-col lg:flex-row gap-8 m-5'>
+      <div className='lg:w-1/2'>
+        <h1 className='font-bold text-center mb-5'>Order Summary</h1>
+        <div className="overflow-x-auto">
+          <table className='min-w-full mx-auto h-[50vh]'>
+            <thead>
+              <tr>
+                <th className='px-4 py-2'>Item</th>
+                <th className='px-4 py-2'>Image</th>
+                <th className='px-4 py-2'>Price</th>
+                <th className='px-4 py-2'>Quantity</th>
+                <th className='px-4 py-2'>Amount</th>
+              </tr>
+            </thead>
+            <tbody className='text-center'>
+              {cartItems.products?.map((item) => (
+                <tr className='border-b-2 font-semibold' key={item.product._id}>
+                  <td className='px-4 py-2'>{item.product.name}</td>
+                  <td className='px-4 py-2'>
+                    <div className='flex justify-center'>
+                      <img src={"https://technotronix-api-eeve.onrender.com/" + item.product.img} className='h-[50px]' alt="" />
                     </div>
-                    <div className='flex flex-col gap-3 mb-3'>
-                        <label htmlFor="last name"> Last Name</label>
-                        <input className='outline outline-1' type="text" name='lastName' />
-                    </div>
-                    <div className='flex flex-col gap-3 mb-3'>
-                        <label htmlFor="phone number"> Phone Number</label>
-                        <input className='outline outline-1' type="text" name='phone' />
-                    </div>
-                    <div className='flex flex-col gap-3 mb-3'>
-                        <label className='font-bold' htmlFor="Address">Address</label>
-                        <textarea className='outline outline-1' name="address" id="" cols="30" rows="10" ></textarea>
-
-                    </div>
-                    <div>
-                        <button className='bg-black text-white p-[10px] rounded-md hover:bg-orange-500'>
-                            Pay Now
-                        </button>
-                    </div>
-                </form>
-            </div>
-
+                  </td>
+                  <td className='px-4 py-2'>₦{item.product.price}</td>
+                  <td className='px-4 py-2'>{item.quantity}</td>
+                  <td className='px-4 py-2'>₦{item.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    )
+        <div className='w-[90%] mx-auto font-bold text-2xl mt-4'>
+          <h1>Total = ₦{totalAmount()}</h1>
+        </div>
+      </div>
+      <div className='lg:w-1/2 mx-auto font-semibold'>
+        <h1 className='text-2xl font-bold mb-5'>Delivery Information</h1>
+        <form onSubmit={handleCheckout} id='orderId'>
+          <div className='flex flex-col gap-3 mb-3'>
+            <label htmlFor="firstName">First Name</label>
+            <input className='outline outline-1 p-2' type="text" name='firstName' required />
+          </div>
+          <div className='flex flex-col gap-3 mb-3'>
+            <label htmlFor="lastName">Last Name</label>
+            <input className='outline outline-1 p-2' type="text" name='lastName' required />
+          </div>
+          <div className='flex flex-col gap-3 mb-3'>
+            <label htmlFor="phone">Phone Number</label>
+            <input className='outline outline-1 p-2' type="text" name='phone' required />
+          </div>
+          <div className='flex flex-col gap-3 mb-3'>
+            <label htmlFor="address" className='font-bold'>Address</label>
+            <textarea className='outline outline-1 p-2' name="address" id="" cols="30" rows="5" required></textarea>
+          </div>
+          <div>
+            <button className='bg-black text-white py-2 px-4 rounded-md hover:bg-orange-500 transition-all'>
+              Pay Now
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
